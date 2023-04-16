cb89a308d67fbda67a2551f97c7886fb1454f495
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-04-16 15:11:21 +0500

    cart page created

--- frontend/Components/CartItem.jsx
+++ frontend/Components/CartItem.jsx
@@ -1,5 +1,58 @@
+import Image from "next/image";
 import React from "react";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 export default function CartItem() {
-  return <div>CartItem</div>;
+  return (
+    <div className="flex py-5 gap-3 md:gap-5 border-b">
+      {/*Image Starts */}
+      <div className="aspect-square w-[50px] md:w-[150px]">
+        <Image src="/product_1.jpg" alt="" width={150} height={150} />
+      </div>
+      {/*Image Ends */}
+      {/*Product Detail Box Starts */}
+      <div className="flex w-full flex-col">
+        {/*Product Name and Price */}
+        <div className="flex flex-col justify-between md:flex-row">
+          <div className="flex font-playfair flex-col font-semibold text-black/[0.8] md:flex-row">
+            Coccinele Handbag
+          </div>
+          <div className="md:text-md block font-quickSand text-sm font-medium text-black/[0.5] md:hidden">
+            The Perfect Accessory for Everyday Use
+          </div>
+          <div className="md:text-md mt-2 text-sm font-bold font-playfair text-black/[0.5]">
+            $ 45.99
+          </div>
+        </div>
+        {/*Product Subtitles displayed when in large screen below the name */}
+        <div className="md:text-md hidden font-medium font-quickSand text-black/[0.5] md:block">
+          The Perfect Accessort for Everyday Use
+        </div>
+        {/*Quantity Selectoin */}
+        <div className="mt-4 flex items-center justify-between">
+          <div className="itens-center md:text-md flex gap-2 text-sm text-black/[0.5] md:gap-10">
+            <div className="flex items-center gap-1">
+              {/*Quantity selectoin */}
+              <div className="font-semibold font-quickSand">Quantity:</div>
+              <select name="" id="">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+              </select>
+            </div>
+          </div>
+          {/*Quantity Selection ends */}
+          {/*Delete Button */}
+          <RiDeleteBin6Line className="cursor-pointer text-black/[0.5] hover:text-black text-[16px]" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,20 +1,56 @@
+import CartItem from "@/Components/CartItem";
 import React from "react";
 
 export default function cart() {
   return (
-    <div className="w-full md:py-20">
-      <section className="max-w-[1200px] mx-auto">
-        <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
-          <div className="text-[28px] md:text-[34px] font-semibold mb-5 leading-tight">
-            Shopping Cart
-          </div>
+    <div className="w-fill md:py-20">
+      <section className="mx-auto max-w-[1200px]">
+        {/*Heading Start */}
+        <div className="mx-auto mt-8 max-w-[800px] text-center md:mt-0 mb-5 text-[28px] font-playfair font-semibold leading-tight md:text-[34px]">
+          Shopping Cart
+          {/*Heading End */}
         </div>
-
-        <div className="flex flex-col lg:flex-row gap-12 py-12">
+        <div className="flex flex-col gap-12 py-10 lg:flex-row">
+          {/*Left Side Box */}
           <div className="flex-[2]">
-            <div className="text-lg font-semibold">Cart Items</div>
+            <div className="text-lg font-bold font-playfair">Cart Items</div>
+            <CartItem />
           </div>
+          {/*Left Side Box End */}
+          {/*Right Box */}
+          <div className="flex-[1]">
+            {/*Summary Heading */}
+            <div className="text-lg font-bold font-playfair">Summary</div>
+            {/*Sumamry Box (Right Box) */}
+            <div className="my-5 rounded-xl bg-black/[0.05] p-5">
+              {/*Subtotal and price */}
+              <div className="flex justify-between">
+                <div className="text-md font-medium uppercase font-playfair text-black md:text-lg">
+                  Subtotal
+                </div>
+                <div className="text-md font-medium text-black md:text-lg font-playfair">
+                  $ 45.99
+                </div>
+              </div>
+              {/*Description */}
+              <div className="text-[14px] md:text-[16px] py-5 border-t mt-5 font-quickSand">
+                The subtotal reflects the total price of your order, including
+                duties and taxes, before any applicable discounts. It doesnt
+                include delievery costs and international transaction fees.
+              </div>
+            </div>
+            {/*Summary Box Ends */}
+            {/*Checkout Button */}
+            <button
+              className="w-full py-4 font-playfair rounded-full bg-black text-white text-lg font-medium transition-transform active:sacle-95 mb-3 hover:opacity-75 
+            flex items-center gap-2 justify-center"
+            >
+              Checkout
+            </button>
+          </div>
+          {/*Right Box ends */}
         </div>
+        {/*Checkout Detail Ends */}
       </section>
     </div>
   );
