4059f8f3a0b315ed63d00a99654d6b0804ba4c0e
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2023-05-01 17:26:02 +0500

    commenting complete

--- backend/src/api/order/controllers/order.js
+++ backend/src/api/order/controllers/order.js
@@ -4,13 +4,22 @@ const stripe = require("stripe")(process.env.STRIPE_KEY);
  * order controller
  */
 
-const { createCoreController } = require("@strapi/strapi").factories;
+const { createCoreController } =
+  require("@strapi/strapi").factories; /*create controller is a factory function that creates a new controller instances for
+a specific contrent type or plugin. By using it, it allows the application to create new controller instance dynamically at runtime */
 
 module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async create(ctx) {
-    const { products } = ctx.request.body;
+    /*handles the creation of new orders when an API request is made. It takes a ctx (content) argument, which is an object containing
+  information about the request */
+    const { products } =
+      ctx.request
+        .body; /*extracts the products property from the request body, which contains an array of objects representing the
+    products that the user want to oder */
     try {
       const lineItems = await Promise.all(
+        /*creates an array of lineItems that represents the individual products that the user wants to order. 'Promise.all'
+      function is used to execute a map function on each product object in parallel. */
         products.map(async (product) => {
           const item = await strapi
             .service("api::product.product")
